a246c5b14dfa468a4b9360325b63f3dac6456223
Rubén <rupacrod@gmail.com>
2020-01-06 20:33:06 +0100

    first test with component

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WelcomeMessage from './welcome/WelcomeMessage';
 
 const App = () => {
   return (
     <div id="app">
       <h2>App Component!</h2>
+      <WelcomeMessage imgPath="https://picsum.photos/200" />
     </div>
   );
 };
